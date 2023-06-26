a9570309962bca3c988083122e0044874cdf1ff4
Franck <41587017+Freddyede@users.noreply.github.com>
2023-06-26 20:32:55 +0200

    Revert "first commit"
    This reverts commit a70c44f9
    
    Signed-off-by: Franck <41587017+Freddyede@users.noreply.github.com>

--- bin/init/templates.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* Templates :
- *  This files contains all templates configurations of project like this => [];
-**/
-
--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once '../autoload.php';
-
-use Core\Kernel\Kernel;
-
-new Kernel();
