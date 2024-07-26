22de20bac465609930ed3a9a079278a49da1f6fc
Franck <136079691+Freddyede@users.noreply.github.com>
2024-07-26 16:09:52 +0200

    Remove method from ContactController::index()
    Signed-off-by: Franck <136079691+Freddyede@users.noreply.github.com>

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,7 +10,6 @@ class ContactController extends AbstractController
     #[Route('/contact', name: 'Contact')]
     public function index(): void
     {
-        var_dump(self::getVersion(true));
         self::setHeader('title', 'Contact');
         self::rendererViews('contact/index.php', [
             'controller_name' => 'ContactController@index'
